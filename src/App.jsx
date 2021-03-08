7d668359a0b9db913cc66d0d8c154dd36c804db3
andrey sushkov <62519759+andrey477@users.noreply.github.com>
2021-03-08 19:23:24 +0300

    fixed some design bugs

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,9 @@ import { Header } from "./components/Header/Header";
 import { Main } from "./components/Main/Main";
 
 export const App = () => (
-  <div className="container">
+  <>
     <Header />
     <Main />
     <Footer />
-  </div>
+  </>
 )
